662b3992dae7811707e84747d598d87a347324c9
Shevnin43 <sergpodlec@mail.ru>
2020-03-18 20:10:39 +0300

    #18032020

---
+++ It-univer.Tasks/It-univer.Tasks.Web/Controllers/TaskController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ItUniver.Task.Entities;
+using ItUniver.Task.Manager;
+using Microsoft.AspNetCore.Mvc;
+
+namespace It_univer.Tasks.Web.Controllers
+{
+    public class TaskController : Controller
+    {
+        private readonly ITaskManager taskManager;
+
+        public TaskController(ITaskManager taskManager)
+        {
+            this.taskManager = taskManager;
+        }
+
+        public IActionResult Index()
+        {
+            var tasks = taskManager.GetAll();
+            return View(tasks);
+        }
+        public IActionResult Create()
+        {
+            return View(new TaskBase());
+        }
+
+    }
+}
---
+++ It-univer.Tasks/It-univer.Tasks.Web/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿
+using ItUniver.Task.Manager;
+using ItUniver.Task.Stores;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace It_univer.Tasks.Web
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddTaskCoreServices(this IServiceCollection services)
+        {
+            services.AddSingleton<ITaskStore, TaskMemoryStore>();
+            services.AddTransient<ITaskManager, TaskManager>();
+
+            return services;
+        }
+    }
+}
